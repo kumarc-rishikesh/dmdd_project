29f58389006eaf88eb9eeedf5f5cfb420c132c81
Rishikesh Kumar <86364952+kumarc-rishikesh@users.noreply.github.com>
2023-11-18 23:20:02 -0500

    service req tables populated

--- scipts/maintainence/add_sr.sql
+++ scipts/maintainence/add_sr.sql
@@ -1,44 +1,52 @@
-CREATE OR REPLACE PROCEDURE add_sr(p_lease_id IN NUMBER, p_type IN VARCHAR2) AS
-    v_exists VARCHAR(1):='N';
-    v_name VARCHAR2(50);
-    v_verify NUMBER;
-    NOT_FOUND EXCEPTION;
-    ALREADY_EXISTS EXCEPTION;
+CREATE OR REPLACE PROCEDURE add_sr (
+    p_lease_id IN NUMBER,
+    p_name IN VARCHAR2,
+    p_dep1 IN NUMBER DEFAULT NULL,
+    p_dep2 IN NUMBER DEFAULT NULL,
+    p_dep3 IN NUMBER DEFAULT NULL)
+IS
+    v_dept_ids SYS.ODCINUMBERLIST := SYS.ODCINUMBERLIST();
+    v_request_id NUMBER;
+    ALL_NULL EXCEPTION;
+    SAME_DEPS EXCEPTION;
 BEGIN
-    SELECT 'Y' INTO v_exists FROM LEASE WHERE lease_id = p_lease_id;
-    SELECT RESIDENT_NAME INTO v_name FROM LEASE WHERE lease_id = p_lease_id;
-    SELECT lease_lease_id INTO v_verify FROM service_request WHERE lease_lease_id = p_lease_id;
-
-    IF v_exists = 'N' THEN
-        RAISE NOT_FOUND;
+    IF p_dep1 IS NULL AND p_dep2 IS NULL AND p_dep3 IS NULL THEN
+        RAISE ALL_NULL;
     END IF;
 
-    IF v_verify IS NOT NULL THEN
-        RAISE ALREADY_EXISTS;
+    IF p_dep1 IS NOT NULL THEN
+        v_dept_ids.EXTEND;
+        v_dept_ids(1) := p_dep1;
     END IF;
 
-    IF p_type = 'Electric' THEN
-        INSERT INTO service_request (REQUEST_ID, LEASE_LEASE_ID, TYPE, DEPT, STATUS, RESIDENT_NAME)
-        VALUES (SERVICE_REQUEST_ID_SEQ.NEXTVAL, p_lease_id, p_type, 1,'OPEN', v_name);
-        DBMS_OUTPUT.PUT_LINE('Your Electric Maintenance is reported and will be assisted soon! Thank you :)');
-    ELSIF p_type = 'Plumbing' THEN
-        INSERT INTO service_request (REQUEST_ID, LEASE_LEASE_ID, TYPE, DEPT,STATUS, RESIDENT_NAME)
-        VALUES (SERVICE_REQUEST_ID_SEQ.NEXTVAL, p_lease_id, p_type, 2,'OPEN', v_name);
-        DBMS_OUTPUT.PUT_LINE('Your Plumbing Maintenance is reported and will be assisted soon! Thank you :)');
-    ELSIF p_type = 'General' THEN
-        INSERT INTO service_request (REQUEST_ID, LEASE_LEASE_ID, TYPE, DEPT,STATUS, RESIDENT_NAME)
-        VALUES (SERVICE_REQUEST_ID_SEQ.NEXTVAL, p_lease_id, p_type, 3,'OPEN', v_name);
-        DBMS_OUTPUT.PUT_LINE('Your General Maintenance is reported and will be assisted soon! Thank you :)');
+    IF p_dep2 IS NOT NULL THEN
+        v_dept_ids.EXTEND;
+        v_dept_ids(2) := p_dep2;
     END IF;
+
+    IF p_dep3 IS NOT NULL THEN
+        v_dept_ids.EXTEND;
+        v_dept_ids(3) := p_dep3;
+    END IF;
+
+    SELECT SERVICE_REQUEST_ID_SEQ.NEXTVAL INTO v_request_id FROM DUAL;
+    INSERT INTO SERVICE_REQUEST (request_id, LEASE_lease_id, type, status, scheduled_for,resident_name)
+    VALUES (v_request_id, p_lease_id, 'resident_raised', 'open' , SYSDATE+5 ,p_name);
+
+    FOR i IN 1..v_dept_ids.COUNT LOOP
+        INSERT INTO SR_DEPT (sr_dept_id, DEPARTMENT_dept_id, SERVICE_REQUEST_request_id)
+        VALUES (SR_DEPT_ID_SEQ.NEXTVAL, v_dept_ids(i), v_request_id);
+        DBMS_OUTPUT.PUT_LINE('Data inserted successfully.');
+    END LOOP;
 
     COMMIT;
-
 EXCEPTION
-    WHEN NOT_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('Invalid Lease ID');
-    WHEN ALREADY_EXISTS THEN
-        DBMS_OUTPUT.PUT_LINE('There is already a request submitted, Please submit your request post completion');
+    WHEN ALL_NULL THEN
+        DBMS_OUTPUT.PUT_LINE('ALL 3 DEPARTMENTS ARE NULL');
+    WHEN SAME_DEPS THEN
+        DBMS_OUTPUT.PUT_LINE('DEPARTMENTS MUST BE DISTINCT');
     WHEN OTHERS THEN
-        DBMS_OUTPUT.PUT_LINE(SQLERRM);
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+        ROLLBACK;
 END add_sr;
 /
