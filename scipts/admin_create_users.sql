3092b06204817c4b5b7a5f367bff447bb282821c
Rishikesh Kumar <86364952+kumarc-rishikesh@users.noreply.github.com>
2023-12-03 00:22:49 -0500

    rerunnable user & role creation

--- scipts/admin_create_users.sql
+++ scipts/admin_create_users.sql
@@ -1,18 +1,60 @@
+SET SERVEROUTPUT ON;
+BEGIN
+  FOR user_rec IN (
+    SELECT username FROM dba_users
+    WHERE cloud_maintained = 'NO'
+      AND username != 'ADMIN'
+      AND username != 'ADB_APP_STORE'
+  )
+  LOOP
+    BEGIN
+      EXECUTE IMMEDIATE 'DROP USER ' || user_rec.username || ' CASCADE';
+      DBMS_OUTPUT.PUT_LINE('Dropped user: ' || user_rec.username);
+    EXCEPTION
+      WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error dropping user ' || user_rec.username || ': ' || SQLERRM);
+    END;
+  END LOOP;
+
+  FOR role_rec IN (
+    SELECT role FROM dba_roles
+    WHERE oracle_maintained = 'N'
+  )
+  LOOP
+    BEGIN
+      EXECUTE IMMEDIATE 'DROP ROLE ' || role_rec.role;
+      DBMS_OUTPUT.PUT_LINE('Dropped role: ' || role_rec.role);
+    EXCEPTION
+      WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error dropping role ' || role_rec.role || ': ' || SQLERRM);
+    END;
+  END LOOP;
+
+  COMMIT; 
+END;
+/
+
+
+CREATE ROLE leasing_office_role ; 
+GRANT CREATE TABLE TO leasing_office_role;
+GRANT CONNECT TO leasing_office_role;
+GRANT RESOURCE TO leasing_office_role;
+GRANT CREATE SEQUENCE TO leasing_office_role;
+GRANT CREATE VIEW TO leasing_office_role;
+GRANT CREATE PROCEDURE TO leasing_office_role;
+
+CREATE ROLE resident_role ;
+GRANT CONNECT TO resident_role;
+GRANT RESOURCE TO resident_role;
+
+CREATE ROLE maintenence_role ; 
+GRANT CONNECT TO maintenence_role;
+GRANT RESOURCE TO maintenence_role;
+
+
 CREATE USER leasing_office IDENTIFIED BY "RootPassword123*"; 
-GRANT CREATE TABLE TO leasing_office;
-GRANT CONNECT TO leasing_office;
-GRANT RESOURCE TO leasing_office;
-GRANT UNLIMITED TABLESPACE TO leasing_office;
-GRANT CREATE SEQUENCE TO leasing_office;
-GRANT CREATE VIEW TO leasing_office;
-GRANT CREATE PROCEDURE TO leasing_office;
-
+GRANT leasing_office_role TO leasing_office;
 CREATE USER resident IDENTIFIED BY "RootPassword123*"; 
-GRANT CONNECT TO resident;
-GRANT RESOURCE TO resident;
-GRANT UNLIMITED TABLESPACE TO resident;
-
-CREATE USER maintainence IDENTIFIED BY "RootPassword123*"; 
-GRANT CONNECT TO maintainence;
-GRANT RESOURCE TO maintainence;
-GRANT UNLIMITED TABLESPACE TO maintainence;
+GRANT resident_role TO resident;
+CREATE USER maintenence IDENTIFIED BY "RootPassword123*"; 
+GRANT maintenence_role TO maintenence;
