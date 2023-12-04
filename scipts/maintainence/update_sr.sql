1762d882daf8c383159cc9ddb6e9cc15b01dde57
Ram <145064995+ramchitrala@users.noreply.github.com>
2023-12-03 19:10:06 -0500

    Update update_sr.sql
    STRUCTURE CHANGED:
    1. Previously the code used to take values of request ID and status but another UDP is handling the updation so this UDP inputs request ID to close the SR.
    2. renaming UDP to UDP_CLOSE_SR

--- scipts/maintainence/update_sr.sql
+++ scipts/maintainence/update_sr.sql
@@ -1,12 +1,12 @@
-CREATE OR REPLACE PROCEDURE update_sr(p_request_id IN NUMBER, p_status IN VARCHAR2) AS
-    v_status VARCHAR2(50) := 'COMPLETED';
+CREATE OR REPLACE PROCEDURE close_sr(p_request_id IN NUMBER) AS
+    v_status VARCHAR2(50):= 'COMPLETED';
     v_exists VARCHAR2(1) := 'N';
     NOT_FOUND EXCEPTION;
 BEGIN
 SELECT 'Y' INTO v_exists FROM SERVICE_REQUEST WHERE REQUEST_ID = p_request_id;
 IF v_exists = 'Y' THEN
     UPDATE SERVICE_REQUEST
-    SET STATUS = v_status,
+    SET STATUS = UPPER(v_status),
     COMPLETED_AT = SYSDATE
     WHERE REQUEST_ID = p_request_id;
     COMMIT;
@@ -18,5 +18,5 @@ WHEN NOT_FOUND THEN
         DBMS_OUTPUT.PUT_LINE('Service Request not found');
 WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Invalid Request Number');     
-END update_sr;
+END close_sr;
 /
