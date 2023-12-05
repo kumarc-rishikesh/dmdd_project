3c678ce3886bf2d0af32d2a24feeba75d9b56814
Rishikesh Kumar <86364952+kumarc-rishikesh@users.noreply.github.com>
2023-12-05 17:50:47 -0500

    final changes.

--- scipts/create_reports.sql
+++ scipts/create_reports.sql
@@ -59,7 +59,6 @@ LEFT JOIN
 GROUP BY
     E.EMPLOYEE_ID, E.EMPLOYEE_NAME, D.NAME;
     
-=======
 CREATE OR REPLACE VIEW RESIDENT_SR AS 
 SELECT request_id,resident_name,type,status,NVL(TO_CHAR(scheduled_for, 'YYYY-MM-DD'), 'TBA') AS scheduled_for,NVL(TO_CHAR(completed_at, 'YYYY-MM-DD'), 'TBA') AS completed_at
 FROM service_request
--- scipts/views/view_owner_getdues.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE VIEW OWNER_VIEW AS
-    WITH dues AS(
-    SELECT l.room_no, l.lease_id, l.rent+(u.utility_cost*u.curr_cycle_units)+v.penalty as pending_dues from lease l
-    INNER JOIN utility u ON l.lease_id = u.lease_lease_id
-    INNER JOIN violations v on l.lease_id = v.lease_lease_id)
-    SELECT  l.owner_owner_id owner_id, l.lease_id, l.room_no, d.pending_dues 
-    FROM lease l
-    INNER JOIN dues d 
-    ON l.lease_id = d.lease_id;
