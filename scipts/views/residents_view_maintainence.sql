039e4c48202ce9d8147cf0330672bd7a41d61ab9
Ram <145064995+ramchitrala@users.noreply.github.com>
2023-12-04 23:55:39 -0500

    Update residents_view_maintainence.sql

--- scipts/views/residents_view_maintainence.sql
+++ scipts/views/residents_view_maintainence.sql
@@ -1,3 +1,4 @@
+CREATE OR REPLACE VIEW RESIDENT_SR AS 
 SELECT
     request_id,
     resident_name,
@@ -5,4 +6,7 @@ SELECT
     status,
     NVL(TO_CHAR(scheduled_for, 'YYYY-MM-DD'), 'TBA') AS scheduled_for,
     NVL(TO_CHAR(completed_at, 'YYYY-MM-DD'), 'TBA') AS completed_at
-FROM service_request;
+FROM
+    service_request
+WHERE
+    completed_at IS NULL;
