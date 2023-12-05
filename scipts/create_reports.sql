2e20f7fa57187cfb927891b92683f1931e514673
Rishikesh Kumar <86364952+kumarc-rishikesh@users.noreply.github.com>
2023-12-05 03:46:54 -0500

    minor changes

--- scipts/create_reports.sql
+++ scipts/create_reports.sql
@@ -8,4 +8,4 @@ ON ab.amenity_amenity_id = A.amenity_id;
 CREATE OR REPLACE VIEW RESIDENT_UTILITY_USAGE AS
 select l.lease_id, u.utility_name, u.curr_cycle_units*u.utility_cost as curr_cost, u.cycle_billed_on from utility u 
 inner join lease l
-on u.lease_lease_id = l.lease_id;
+on u.lease_lease_id = l.lease_id; 
