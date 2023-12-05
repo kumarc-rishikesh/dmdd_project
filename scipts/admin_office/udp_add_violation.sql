358ce8552834c9cd7f6e718a547534cf2ddb2590
Pranav R.S <rajavelusivakumar.p@northeastern.edu>
2023-12-05 11:20:26 -0500

    setting rent_status when violations are added

--- scipts/admin_office/udp_add_violation.sql
+++ scipts/admin_office/udp_add_violation.sql
@@ -1,4 +1,9 @@
-CREATE OR REPLACE PROCEDURE ADD_VIOLATION(
+-- Check if the penalty is not null and update pending dues accordingly
+-- If pending dues are not null, add the penalty to the existing amount and update rent status
+                
+                
+SET SERVEROUTPUT ON
+CREATE OR REPLACE PROCEDURE add_violation(
     PI_LEASE_ID VIOLATIONS.LEASE_LEASE_ID%TYPE,
     PI_PENALTY NUMBER,
     PI_TYPE VARCHAR2
@@ -9,34 +14,30 @@ AS
     v_lease_end_date DATE;
     E_LEASE_NOT_ACTIVE EXCEPTION;
 BEGIN
-    -- Fetch lease details
     SELECT PENDING_DUES, PENDING_DUE_ON, END_DATE INTO v_current_pending_dues, v_current_pending_due_on, v_lease_end_date
     FROM LEASE
     WHERE LEASE_ID = PI_LEASE_ID;
 
-    -- Check if the lease is active
     IF v_lease_end_date < SYSDATE THEN
         RAISE E_LEASE_NOT_ACTIVE;
     END IF;
 
-    -- Insert the violation
     INSERT INTO VIOLATIONS (
         VIOLATION_ID, LEASE_LEASE_ID, PENALTY, TYPE
     ) VALUES (
         violation_id_seq.NEXTVAL, PI_LEASE_ID, PI_PENALTY, PI_TYPE
     );
 
-    -- Check if the penalty is not null and update pending dues accordingly
+   
+
     IF PI_PENALTY IS NOT NULL THEN
         IF v_current_pending_dues IS NULL THEN
-            -- If pending dues are null, set it to the penalty
-            UPDATE LEASE SET PENDING_DUES = PI_PENALTY, DUES_LAST_CLEARED = SYSDATE WHERE LEASE_ID = PI_LEASE_ID;
+            -- If pending dues are null, set it to the penalty and update rent status
+            UPDATE LEASE SET PENDING_DUES = PI_PENALTY, RENT_STATUS = 'Unpaid', DUES_LAST_CLEARED = SYSDATE WHERE LEASE_ID = PI_LEASE_ID;
         ELSE
-            -- If pending dues are not null, add the penalty to the existing amount
-            UPDATE LEASE SET PENDING_DUES = PENDING_DUES + PI_PENALTY, DUES_LAST_CLEARED = SYSDATE WHERE LEASE_ID = PI_LEASE_ID;
+            UPDATE LEASE SET PENDING_DUES = PENDING_DUES + PI_PENALTY, RENT_STATUS = 'Unpaid', DUES_LAST_CLEARED = SYSDATE WHERE LEASE_ID = PI_LEASE_ID;
         END IF;
         
-        -- Update pending_due_on
         IF v_current_pending_due_on IS NULL THEN
             UPDATE LEASE SET PENDING_DUE_ON = ADD_MONTHS(SYSDATE, 1) WHERE LEASE_ID = PI_LEASE_ID;
         ELSE
@@ -52,8 +53,9 @@ EXCEPTION
         DBMS_OUTPUT.PUT_LINE('Lease ID not found.');
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
-END ADD_VIOLATION;
+END add_violation;
 /
+
 
 
 
@@ -70,5 +72,5 @@ EXEC ADD_VIOLATION('51', 500, 'Pet');
 
 
 
---delete from violations;
+--delete from violations where lease_lease_id=51;
 --commit;
